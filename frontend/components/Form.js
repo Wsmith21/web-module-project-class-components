e3d55e234959b141ce63b6bf9bb916cf755e5221
walyn smithe <walynsmithe@yahooe.com>
2023-11-03 11:06:11 -0500

    updated

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+onSubmit =evt => {
+  evt.preventDefault()
+  this.props.addTodo(this.state.name)
+  this.setState({
+    ...this.state,
+    name: ''
+  })
+}
+onChange =evt => {
+  const {value} = evt.target
+  this.setState({
+    name: value
+    ...this.state,
+  })
+}
   render() {
+   
     return (
       <div>
-        Form
+        <Form onsubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onChange}/>
+          <input type="submit" />
+        </Form>
+        
       </div>
     )
   }
